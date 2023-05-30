2fa629f2bad628a93ada10400742f3b199ba09fc
LucSan01 <osakweluckychukwudi@gmail.com>
2023-05-30 15:43:21 -0700

    0-reset_to_98.c correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * rest_to_98 - Takes a pointer to an int and updates
- * the vlue it points to 98
+ *rest_to_98 - Takes a pointer to an int and updates
+ *the vlue it points to 98
  *
- * @n: An integer input pointer
+ *@n: An integer input pointer
  *
- * Return : noting
+ *Return : noting
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+ *n = 98;
 }
